194f1b2972b0aba1f19a30b1c495d8d02ebe7839
Edd113388 <ed_stegmaier@hotmail.com>
2026-07-09 00:49:44 -0400

    test

--- src/main/java/com/duoc/gestionpedidos/config/RabbitMQConfig.java
+++ src/main/java/com/duoc/gestionpedidos/config/RabbitMQConfig.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Configuration
 public class RabbitMQConfig {
 
@@ -36,8 +38,8 @@ public class RabbitMQConfig {
     private String password;
 
     @Bean
-    Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+    Jackson2JsonMessageConverter messageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 
     @Bean
